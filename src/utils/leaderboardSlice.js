0eed73d77d2e6a0facf2cc3eaa8a6b13f3a7af17
Rakeshvanam6868 <rakeshvanam6868@gmail.com>
2024-07-18 13:37:22 +0530

    changed dummy data

--- src/utils/leaderboardSlice.js
+++ src/utils/leaderboardSlice.js
@@ -2,16 +2,16 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   entries: [
-    {id:1, username: 'Rishi', score: '00:56:23' },
-    {id:2, username: 'Harsha', score: '00:56:23' },
-    {id:3, username: 'Prabhas', score: '00:56:23' },
-    {id:4, username: 'Dibas', score: '00:56:23' },
-    {id:5, username: 'Chidu', score: '00:56:23' },
-    {id:6, username: 'Rohit', score: '00:56:23' },
-    {id:7, username: 'Yashwant', score: '00:56:23' },
-    {id:8, username: 'Sai', score: '00:56:23' },
-    {id:9, username: 'Sai Krishna', score: '00:56:23' },
-    {id:10, username: 'Adithya', score: '00:56:23' },
+    {id:1, username: 'Rohit', score: '00:56:23' },
+    {id:2, username: 'Kohli', score: '00:56:23' },
+    {id:3, username: 'Pant', score: '00:56:23' },
+    {id:4, username: 'Sky', score: '00:56:23' },
+    {id:5, username: 'Dube', score: '00:56:23' },
+    {id:6, username: 'Hardhik', score: '00:56:23' },
+    {id:7, username: 'Axar', score: '00:56:23' },
+    {id:8, username: 'Kuldeep', score: '00:56:23' },
+    {id:9, username: 'Arshadeep', score: '00:56:23' },
+    {id:10, username: 'Bumrah', score: '00:56:23' },
   ],
   recentEntry: null,
 };
